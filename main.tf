2a14dc5a114d5b39c73ce9f1a4f504a1a49aac20
Ramtin Safadoust <ramtin.safadoust@gmail.com>
2023-02-16 12:06:45 +0330

    seperate provider file

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.13"
-    }
-  }
-}
 
-provider "proxmox" {
-  pm_debug            = true
-  pm_api_url          = var.api_url
-  pm_api_token_id     = var.api_token_id
-  pm_api_token_secret = var.api_token_secret
-  pm_tls_insecure     = true
-}
 
 
 
